9a7adde6a36baf430d8a50ed58ee1a2d9345e7ea
ZeoXer <andrew17413@gmail.com>
2022-06-04 00:51:47 +0800

    20220604

--- chatTalker/js/all.js
+++ chatTalker/js/all.js
@@ -1,8 +1,3 @@
 $(document).ready(function () {
-  // $('.back-to-top').click(function (e) {
-  //   e.preventDefault();
-  //   $('html, body').animate({
-  //       scrollTop: 0
-  //     }, 800)
-  // });
+
 });
