483b64af97a96916d807722aac4cdab0320af8e0
ZeoXer <andrew17413@gmail.com>
2022-09-01 23:06:53 +0800

    settings upload

---
+++ TodoListAPI/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/js/all.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
